4446f76f1539f637ed9b849282c9745c41085887
Tystr12 <73970359+Tystr12@users.noreply.github.com>
2024-09-12 15:42:27 +0200

    fixed search bar and fonts

--- dadsbooks/books/templates/books/script.js
+++
@@ -1,6 +0,0 @@
-function searchbar() {
-    let text = document.getElementById("search");
-    text.classList.toggle("hide");
-    text.classList.toggle("show");
-    console.log("triigered searchbar function.")
-}
