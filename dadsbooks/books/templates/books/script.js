df3c03d7dc1d17ca984354071d234265d31e29f2
Tystr12 <73970359+Tystr12@users.noreply.github.com>
2024-09-12 14:48:29 +0200

    working on fonts

---
+++ dadsbooks/books/templates/books/script.js
@@ -0,0 +1,6 @@
+function searchbar() {
+    let text = document.getElementById("search");
+    text.classList.toggle("hide");
+    text.classList.toggle("show");
+    console.log("triigered searchbar function.")
+}
